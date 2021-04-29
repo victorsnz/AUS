9dc8975039d8c55dfab87b50b4e9cf703be2331f
Victor Sanchez <victor_sanchez@outlook.com>
2021-04-29 18:49:17 -0300

    App router and styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,30 @@
-import './App.css';
+import React from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./App.css";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import NavigationBar from "./components/NavigationBar/NavigationBarComponent";
+import Home from "./components/Home/HomeComponent";
+import SuperHeroes from "./components/Superheroes/SuperheroesComponent";
+import SuperPowers from "./components/SuperPowers/SuperPowersComponent";
+import Misions from "./components/Misions/MisionsComponent";
+import Footer from "./components/Footer/FooterComponent";
 
 function App() {
   return (
-    <div className="App">
-      Hello React!
-    </div>
+    <Router>
+      <NavigationBar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/superheroes" component={SuperHeroes} />
+        <Route path="/superpoderes" component={SuperPowers} />
+        <Route path="/misiones" component={Misions} />
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
